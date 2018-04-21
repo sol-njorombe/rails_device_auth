e42155a8cd15d51ede23457275739856be45bc45
NJOROMBE <njorombe@gmail.com>
2018-04-20 22:46:14 -0500

    admin permission fix

--- db/migrate/20180408173635_create_admin_permissions.rb
+++ db/migrate/20180408173635_create_admin_permissions.rb
@@ -3,9 +3,9 @@ class CreateAdminPermissions < ActiveRecord::Migration[5.0]
     create_table :admin_permissions do |t|
       t.references :admin, foreign_key: true
       t.references :component, foreign_key: true
-      t.boolean :create :default => false
-      t.boolean :update :default => false
-      t.boolean :delete :default => false
+      t.boolean :create, :default => false
+      t.boolean :update, :default => false
+      t.boolean :delete, :default => false
       t.references :updated_by, index: true, foreign_key: { to_table: :users }
       t.timestamps
     end
